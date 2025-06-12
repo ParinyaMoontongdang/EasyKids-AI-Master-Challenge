166c6c03ccf6984b9e7c972e4c3f7cd2793460a5
Parinya(Petong) <90907279+ParinyaMoontongdang@users.noreply.github.com>
2025-06-12 19:40:31 +0700

    Update servo chanel

--- arduino/Servo_Smooth/Servo_Smooth.ino
+++ arduino/Servo_Smooth/Servo_Smooth.ino
@@ -4,6 +4,7 @@ int i;
 
 void setup() {
   EasyKids_Setup();
+  display3in1();
   welcomeSong();
   
   waitForStart(); 
@@ -13,13 +14,13 @@ void setup() {
 void loop(){
   for (i = 0; i <= 180; i++)
   {
-    servo(1, i);
+    servo(6, i);
     Serial.println(i);
     delay(20);
   }
   for (i = 180; i >= 0; i--)
   {
-    servo(1, i);
+    servo(6, i);
     Serial.println(i);
     delay(20);
   }
