7f066387f9db6e9a28a4038fdc460aade19a9ea5
Parinya(Petong) <90907279+ParinyaMoontongdang@users.noreply.github.com>
2025-06-17 20:16:48 +0700

    Update tag_families --> image.TAG16H5

--- K210/ApilTag.py
+++ K210/ApilTag.py
@@ -7,20 +7,21 @@ sensor.skip_frames(time = 2000)
 lcd.init()
 
 # <---- tag_families ---->
-#       image.TAG16H5 
+#       image.TAG16H5 ## For EasyKids Ai Car ##
 #       image.TAG25H7 
-#       image.TAG25H9 ## For EasyKids Ai Car ##
+#       image.TAG25H9 
 #       image.TAG36H10 
 #       image.TAG36H11 
 #       image.ARTOOLKIT
 
 while(True):
     img = sensor.snapshot()
-    tag = img.find_apriltags(families=image.TAG25H9)
+    tag = img.find_apriltags(families=image.TAG16H5)
     
     if len(tag) > 0:
         img.draw_string(7,8, str(tag[0].id()), color=(0,128,0), scale=3)
         img.draw_rectangle(tag[0].rect(), color = (255, 0, 0),thickness=3)
         print("Tag ID:", tag[0].id())
-        
-    lcd.display(img)
+    
+    resized = img.resize(320,240)
+    lcd.display(resized)
