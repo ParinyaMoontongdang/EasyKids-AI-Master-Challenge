08a7040d690708c63750b9dd741ae3cfa74ff04f
Parinya(Petong) <90907279+ParinyaMoontongdang@users.noreply.github.com>
2025-06-25 22:23:03 +0700

    Add Guidelines

--- K210/lcd.py
+++ K210/lcd.py
@@ -1,6 +1,8 @@
-import  lcd
+import lcd
 
 lcd.init()
+lcd.clear(lcd.WHITE)
 
 while(True):
+    ## lcd.draw_string(x, y, text, text_color, bg_color) ##
     lcd.draw_string(10, 220, "Hello World!", lcd.BLACK, lcd.GREEN)
